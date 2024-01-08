34a42c2f667cc5e78824f8ab9cd138bb6e5343e2
Pak <pakapongm@gmail.com>
2024-01-08 14:03:10 -0800

    updated middleware

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
+const dashboardRoutes = require('./dashboardRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/dashboard', dashboardRoutes);
+router.use('user', userRoutes);
+
+module.exports = router;
