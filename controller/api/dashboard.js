3fb4db4455f4c416a94e60a2752691a71985b146
Pak <pakapongm@gmail.com>
2024-01-08 13:46:42 -0800

    added auth.js utils and update user model

--- controller/api/dashboard.js
+++ controller/api/dashboard.js
@@ -1,2 +1,14 @@
 const router = require('express').Router();
+const User = require('../../models/User');
 
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+        });
+        res.json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    }
+);
