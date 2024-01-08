fd2f7a8c31cb9388860a5c710427b03c5aeb92df
Pak <pakapongm@gmail.com>
2024-01-08 15:31:33 -0800

    adjusted code in userRoutes to handle strictly login signup signout posts, homeRoute has only getters

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -30,4 +30,18 @@ router.post('/deleteblog', async (req, res) => {
     }
 );
 
+router.post('/editblog', async (req, res) => {
+    try {
+        const blogData = await Blog.update({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newBlog);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    }
+);
+
+
 module.exports = router;
