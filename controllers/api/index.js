ef4d7a5daff91342d9c530ad2a48fb8418bb87ff
Pak <pakapongm@gmail.com>
2024-01-08 16:57:21 -0800

    fixed syntax

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const blogRoutes = require('./blogRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/blog', dashboardRoutes);
+router.use('/blog', blogRoutes);
 router.use('/user', userRoutes);
 
 module.exports = router;
